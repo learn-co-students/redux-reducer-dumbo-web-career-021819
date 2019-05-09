cdad98028e8d1351166c4771eeb295f77f862bd8
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-05-09 13:10:07 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      let newFriend = { name: 'Joe', hometown: 'Boston', id: 101 };
+      return { friends: [...state.friends, newFriend] };
+    case "REMOVE_FRIEND":
+      let newFriends = state.friends.filter(friend => friend.id !== 101);
+      return { friends: newFriends };
+    default:
+      return state;
+  }
 }
